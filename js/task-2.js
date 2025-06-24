d1f7304943f773ecafccc500a240c65f9169853b
V1talik.Ben <magaflurry@gmail.com>
2025-06-25 00:08:08 +0200

    end

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    fxArray = firstArray.concat(secondArray)
+    if (fxArray.length >= maxLength) {
+
+        const cutElement = fxArray.slice(0, maxLength)
+
+        return cutElement
+    } 
+
+return fxArray
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
